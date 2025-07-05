7949f6066a25cdb6b560efd5de9d9b0c085a1219
Asel Turatbek Kyzy <aselturatbekk@gmail.com>
2025-07-05 03:24:41 +0300

    homepage added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,71 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
-import "../styles/Page.scss";
+import { Trans, useTranslation } from "react-i18next";
+import "../styles/Home.scss";
+import devImage from "../assets/images/asel_2.jpeg";
+
+import {
+  FaReact, FaNodeJs, FaHtml5, FaCss3Alt, FaBootstrap,
+  FaGitAlt, FaGithub, FaJsSquare,
+} from "react-icons/fa";
+import {
+  SiTypescript, SiNestjs, SiMongodb,
+  SiMysql, SiFirebase, SiJira
+} from "react-icons/si";
+
+const technologies = [
+  { icon: <FaJsSquare />, name: "JavaScript" },
+  { icon: <SiTypescript />, name: "TypeScript" },
+  { icon: <FaReact />, name: "React.js / Native" },
+  { icon: <FaNodeJs />, name: "Node.js / Express" },
+  { icon: <SiNestjs />, name: "NestJS" },
+  { icon: <SiMongodb />, name: "MongoDB" },
+  { icon: <SiMysql />, name: "MySQL" },
+  { icon: <SiFirebase />, name: "Firebase" },
+  { icon: <FaHtml5 />, name: "HTML5" },
+  { icon: <FaCss3Alt />, name: "CSS3" },
+  { icon: <FaBootstrap />, name: "Bootstrap" },
+  { icon: <FaGitAlt />, name: "Git" },
+  { icon: <FaGithub />, name: "GitHub" },
+  { icon: <SiJira />, name: "Agile / Jira" },
+];
 
 const Home = () => {
   const { t } = useTranslation();
 
   return (
-    <section className="page home-page">
-      <h1>{t("home")}</h1>
-      <p>{t("welcomeMessage")}</p>
+    <section className="home-section">
+      <div className="home-content">
+        <div className="home-text">
+          <h1>
+            <Trans i18nKey="home.title">
+              Full Stack<br />Developer
+            </Trans>
+          </h1>
+          <p>
+            <Trans i18nKey="home.intro">
+              Hi, I’m <strong>Asel Turatbek Kyzy</strong><br />
+              Turning innovative ideas into clean, scalable,<br />
+              and user-focused <i>web & mobile</i> solutions.
+            </Trans>
+          </p>
+        </div>
+        <div className="home-image">
+          <img src={devImage} alt="Asel" />
+        </div>
+      </div>
+
+      <div className="tech-carousel-wrapper">
+        <div className="tech-carousel">
+          <div className="carousel-track">
+            {[...technologies, ...technologies].map((tech, i) => (
+              <div className="tech-item" key={i}>
+                {tech.icon}
+                <span>{tech.name}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
